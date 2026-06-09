6748526978f14a918d144415ccebbdda26371a13
Gruffuss <36421669+Gruffuss@users.noreply.github.com>
2026-06-09 19:50:36 +0300

    Update lampsmart_pro_light.cpp

--- components/lampsmart_pro_light/lampsmart_pro_light.cpp
+++ components/lampsmart_pro_light/lampsmart_pro_light.cpp
@@ -6,7 +6,7 @@
 
 #include <esp_gap_ble_api.h>
 #include <esp_gatts_api.h>
-#ifdef USE_ESP32_FRAMEWORK_ARDUINO\
+#ifdef USE_ESP32_FRAMEWORK_ARDUINO
 #include <esp_wifi.h>
 #include <esp_wifi_types.h>
 #endif
